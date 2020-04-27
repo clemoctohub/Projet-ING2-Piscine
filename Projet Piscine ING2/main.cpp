4aa6eda027563305be59ed6e9edbbbd0c500d6b9
flavienbarreira <45532042+flavienbarreira@users.noreply.github.com>
2020-04-27 10:46:24 +0200

    Essai commentaires

--- Projet Piscine ING2/main.cpp
+++ Projet Piscine ING2/main.cpp
@@ -4,9 +4,9 @@
 
 int main()
 {
-    std::string nomFichier;
-    std::cin>>nomFichier;
-    Graphe mongraphe(nomFichier);
+    /*std::string nomFichier;
+    std::cin>>nomFichier;*/
+    Graphe mongraphe("arrete.txt");
     mongraphe.afficher();
     return 0;
 }
