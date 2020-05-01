c2eb5be551965f0e29bf54b628e7244903c1a9e2
flavienbarreira <45532042+flavienbarreira@users.noreply.github.com>
2020-05-01 12:39:13 +0200

    affichage rentrer fichier

--- Projet Piscine ING2/main.cpp
+++ Projet Piscine ING2/main.cpp
@@ -23,17 +23,61 @@ int menu()  //menu d'affichage
     }
     return choix;
 }
+std::string menu2()  //menu d'affichage
+{
+    int choix;
+    std::string fichier;
+    std::cout << "Choisissez un des fichiers suivants ou rentrer le nom du fichier :" << std::endl;
+    std::cout << std::endl;
+    std::cout << "1: graphe1.txt" << std::endl;
+    std::cout << "2: graphe2.txt" << std::endl;
+    std::cout << "3: graphe3.txt" << std::endl;
+    std::cout << "4: graphe4.txt" << std::endl;
+    std::cout << "5: reseau_ile_reunion.txt" << std::endl;
+    std::cout << "6: Rentrer le nom du fichier a la main" << std::endl;
+    std::cin >> choix;
+    while(choix<1 || choix>6)
+    {
+        std::cout << "Veuillez rentrer une des valeurs demandees" << std::endl;
+        std::cin >> choix;
+    }
+    switch(choix)
+        {
+        case 1:
+            fichier="graphe1.txt";// pour chaque action, nous appellons la methode ou la fonction qui correspond
+            break;
+        case 2:
+            fichier="graphe2.txt";
+            break;
+        case 3:
+            fichier="graphe3.txt";
+            break;
+        case 4:
+            fichier="graphe4.txt";
+            break;
+        case 5:
+            fichier="reseau_ile_reunion.txt";
+            break;
+        case 6:
+            fichier="rien";
+            break;
+        }
+        return fichier;
+}
 
 int main()
 {
     int choix=0;
     std::vector <std::vector <double>> ensemble;
-    std::string nomFichier;    // on permet � l'utilisateur de choisir le fichier qu'il veut ouvrir
-    std::cout << "Rentrez le nom du fichier" << std::endl;
-    std::cin>>nomFichier;
+    std::string nomFichier;
+    nomFichier=menu2();
+    if(nomFichier=="rien") // on permet a l'utilisateur de choisir le fichier qu'il veut ouvrir
+    {
+        std::cout << "Rentrez le nom du fichier" << std::endl;
+        std::cin>>nomFichier;
+    }
     Graphe mongraphe(nomFichier);
-    mongraphe.k_connexite();
-    while(choix!=7)              // switch permettant de choisir l'action voulut en fonction du menu �nonnc� plus haut
+    while(choix!=7)              // switch permettant de choisir l'action voulut en fonction du menu enonnce plus haut
     {
         choix=menu();
         switch(choix)
@@ -49,7 +93,7 @@ int main()
             indicedecentralite(mongraphe,mongraphe.get_ordre(),1);
             break;
         case 4:
-            mongraphe.connexite(1);
+            mongraphe.connexite();
             break;
         case 5:
             indicedecentralite(mongraphe,mongraphe.get_ordre(),0);
