238d14b94ac8a63252d72110193010a3d5cfa544
flavienbarreira <45532042+flavienbarreira@users.noreply.github.com>
2020-05-02 16:21:07 +0200

    truc pas ouf

--- Projet Piscine ING2/bibliotheque.h
+++ Projet Piscine ING2/bibliotheque.h
@@ -1,15 +1,15 @@
 #ifndef BIBLIOTHEQUE_H_INCLUDED
 #define BIBLIOTHEQUE_H_INCLUDED
 
-void boucle(int choix, Graphe mongraphe);
+void menuAfficherIndiceSVG();
 void afficher (std::vector <float> v1);
+void boucle(int choix, Graphe mongraphe);
+void afficher_indice_svg(Graphe mongraphe);
+void afficher_indice(std::vector <std::vector <double>> v1,int ordre);
+int menu(bool m_ponderation);
 std::vector <float> calculdegrenorma(std::vector <float> degresnorma);
 std::vector <std::vector <double>> indicedecentralite(Graphe mongraphe,int ordre, int suppr);
-void afficher_indice(std::vector <std::vector <double>> v1,int ordre);
-void afficher_indice_svg(Graphe mongraphe);
-int menu(bool m_ponderation);
 std::string menu2();
 std::string menuPonderation();
-void menuAfficherIndiceSVG();
 
 #endif // BIBLIOTHEQUE_H_INCLUDED
